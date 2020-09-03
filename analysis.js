4a2b665e02e3d652f4a07ffdbebff7b9f669e2c7
Vitor Lima <vitorfdl@tago.io>
2020-09-03 16:34:06 -0300

    Update analysis.js

--- analysis.js
+++ analysis.js
@@ -1,119 +1,86 @@
 /*
- * Analysis Example
- * Geofences
- *
- * Use geofences to control the area that your devices are in.
- *
- * Instructions
- * To run this analysis you need to add an account token to the environment variables,
- * To do that, go to your account settings, then token and copy your token.
- * 1 - Enter the following link: https://admin.tago.io/account/
- * 2 - Select your Profile.
- * 3 - Enter Tokens tab.
- * 4 - Generate a new Token with Expires Never.
- * 5 - Press the Copy Button and place at the Environment Variables tab of this analysis with key account_token.
- *
- * Follow this guide https://docs.tago.io/en/articles/151 and create
- * two geofences, one with the event code 'danger' and another named 'safe'.
+ ** Analysis Example
+ ** Sending downlink using dashboard
+ **
+ ** Using an Input Widget in the dashboard, you will be able to trigger a downlink to
+ ** any LoraWaN network server.
+ ** You can get the dashboard template to use here: http://admin.tago.io/template/5f514218d4555600278023c4
+ **
+ ** Environment Variables
+ ** In order to use this analysis, you must setup the Environment Variable table.
+ **
+ ** account_token: Your account token. Check bellow how to get this.
+ ** default_PORT: The default port to be used if not sent by the dashboard.
+ **
+ ** Steps to generate an account_token:
+ ** 1 - Enter the following link: https://admin.tago.io/account/
+ ** 2 - Select your Profile.
+ ** 3 - Enter Tokens tab.
+ ** 4 - Generate a new Token with Expires Never.
+ ** 5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
  */
+const { Analysis, Account, Utils } = require('@tago-io/sdk');
+const axios        = require('axios');
 
-const { Utils, Account, Analysis, Device, Services } = require("@tago-io/sdk");
-const geolib = require("geolib");
+async function init(context, scope) {
+  if (!scope[0]) return context.log('This analysis must be triggered by a widget.');
 
+  context.log('Downlink analysis started');
+  // Get the environment variables.
+  const env = Utils.envToJson(context.environment);
+  if (!env.account_token) return context.log('Missing "account_token" environment variable');
+  else if (env.account_token.length !== 36) return context.log('Invalid "account_token" in the environment variable');
 
-// This function checks if our device is inside a polygon geofence
-function insidePolygon(point, geofence) {
-  const x = point[1];
-  const y = point[0];
-  let inside = false;
-  for (let i = 0, j = geofence.length - 1; i < geofence.length; j = i++) {
-    const xi = geofence[i][0];
-    const yi = geofence[i][1];
-    const xj = geofence[j][0];
-    const yj = geofence[j][1];
-    const intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
-    if (intersect) inside = !inside;
+  // Instance the Account class
+  const account = new Account({ token: env.account_token });
+
+  // Get the variables form_payload and form_port sent by the widget/dashboard.
+  const payload = scope.find(x => x.variable === 'form_payload');
+  const port = scope.find(x => x.variable === 'form_port') || { value: env.default_PORT };
+  if (!payload || !payload.value) return context.log('Missing "form_payload" in the data scope.');
+  else if (!port || !port.value) return context.log('Missing "form_port" in the data scope.');
+
+  const device_id = payload.origin; // All variables that trigger the analysis have the "origin" parameter, with the TagoIO Device ID.
+  if (!device_id) return context.log('Device ID <origin> not found in the variables sent by the widget/dashboard.');
+
+  // Find the token containing the authorization code used.
+  const device_tokens = await account.devices.tokenList(device_id, 1, 10, {}, ['serie_number', 'last_authorization']);
+  const token = device_tokens.find(x => x.serie_number && x.last_authorization);
+  if (!token) return context.log("Couldn't find a token with serial/authorization for this device");
+
+  // Get the connector ID from the device
+  const { connector: connector_id } = await account.devices.info(device_id);
+  if (!connector_id) return context.log('Device is not using a connector.');
+
+  // Get the connector information with the NS URL for the Downlink
+  const connector = await account.connector.info(connector_id);
+  if (!connector.options.middleware) return context.log("Couldn't find a connector middleware for this device.");
+
+  // Set the parameters for the device. Some NS like Everynet need this.
+  const params = await account.devices.paramList(device_id);
+  const downlink_param = params.find(x => x.key === 'downlink');
+  if (downlink_param) {
+    await account.devices.paramEdit(device_id, downlink_param.id, { value: payload, sent: false });
+  } else {
+    await account.devices.paramCreate(device_id, { key: 'downlink', value: payload, sent: false });
   }
-  return inside;
+
+  context.log('Trying to send the downlink');
+  const data = {
+    device: token.serie_number,
+    authorization: token.last_authorization,
+    payload: payload.value,
+    port: port.value,
+  };
+
+  await axios.post(`https://${connector.options.middleware}/downlink`, data)
+   .catch((error) => {
+     context.log(`Downlink failed with status ${error.response.status}`);
+     context.log(error.response ? error.response.data : JSON.stringify(error));
+   })
+   .then((result) => {
+     context.log(`Downlink accepted with status ${result.status}`);
+   });
 }
 
-// This function checks if our device is inside any geofence
-function checkZones(point, geofence_list) {
-  // The line below gets all Polygon geofences that we may have.
-  const polygons = geofence_list.filter((x) => x.geolocation.type === "Polygon");
-  if (polygons.length) {
-    // Here we check if our device is inside any Polygon geofence using our function above.
-    const pass_check = polygons.map((x) => insidePolygon(point, x.geolocation.coordinates[0]));
-    const index = pass_check.findIndex((x) => x === true);
-    if (index !== -1) return polygons[index];
-  }
-  // The line below gets all Point (circle) geofences that we may have.
-  const circles = geofence_list.filter((x) => x.geolocation.type === "Point");
-  if (circles.length) {
-    // Here we check if our device is inside any Point geofence using a third party library called geolib.
-    const pass_check = circles.map((x) =>
-      geolib.isPointWithinRadius(
-        { latitude: point[1], longitude: point[0] },
-        { latitude: x.geolocation.coordinates[0], longitude: x.geolocation.coordinates[1] },
-        x.geolocation.radius
-      )
-    );
-    const index = pass_check.findIndex((x) => x);
-    if (index !== -1) return circles[index];
-  }
-  return;
-}
-
-// This function help us get the device using just its id.
-async function getDevice(account, device_id) {
-  const customer_token = await Utils.getTokenByName(account, device_id);
-  const customer_dev = new Device({ token: customer_token });
-  return customer_dev;
-}
-
-async function startAnalysis(context, scope) {
-  context.log("Running");
-
-  if (!scope[0]) throw "Scope is missing"; // doesn't need to run if scope[0] is null
-
-  // The code block below gets all environment variables and checks if we have the needed ones.
-  const environment = Utils.envToJson(context.environment);
-  if (!environment.account_token) throw "Missing account_token environment var";
-
-  const account = new Account({ token: environment.account_token });
-  const device_id = scope[0].origin;
-
-  // Here we get the device information using our account data and the device id.
-  const device = await getDevice(account, device_id);
-
-  // This checks if we received a location
-  const location = scope.find((data) => data.variable === "location");
-  if (!location || !location.location) return context.log("No location found in the scope.");
-
-  // Now we check if we have any geofences to go through.
-  const geofences = await device.getData({ variable: "geofence", qty: 10 });
-  const zones = geofences.map((geofence) => geofence.metadata);
-
-  const zone = checkZones(location.location.coordinates, zones);
-  if (!zone) {
-    // If no geofence is found, we stop our application sending a notification.
-    notification.send({ title: "No zone alert", message: "Your device is not inside any zone." });
-    context.log("Your device is not inside any zone.");
-    return;
-  }
-
-  // The line below starts our notification service.
-  const notification = new Services({ token: context.token }).Notification;
-  if (zone.event === "danger") {
-    // If our device is inside a danger geofence, we will send a notification with a danger alert.
-    notification.send({ title: "Danger alert", message: "Your device is inside a dangerous zone." });
-  }
-  if (zone.event === "safe") {
-    // If our device is inside a safe geofence, we will send a safe geofence notification.
-    notification.send({ title: "Sage alert", message: "Your device is inside a safe zone." });
-  }
-
-  context.log(zone.event);
-}
-
-module.exports = new Analysis(startAnalysis);
+module.exports = new Analysis(init);
