b9577c448db329ecd35fd3859399a25fc1b0167c
Vitor Lima <vitorfdl@tago.io>
2022-06-30 00:15:53 -0300

    Update analysis.js
    Code updated to use Utils.sendDownlink, in order to improve error handling.

--- analysis.js
+++ analysis.js
@@ -5,6 +5,7 @@
  ** Using an Input Widget in the dashboard, you will be able to trigger a downlink to
  ** any LoraWaN network server.
  ** You can get the dashboard template to use here: http://admin.tago.io/template/5f514218d4555600278023c4
+ ** IMPORTANT: Your device is required to send an Uplink before any downlink can be send.
  **
  ** Environment Variables
  ** In order to use this analysis, you must setup the Environment Variable table.
@@ -21,65 +22,62 @@
  ** 4 - Generate a new Token with Expires Never.
  ** 5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
  */
-const { Analysis, Account, Utils } = require('@tago-io/sdk');
-const axios        = require('axios');
+const { Analysis, Account, Utils } = require("@tago-io/sdk");
 
 async function init(context, scope) {
-  if (!scope[0]) return context.log('This analysis must be triggered by a widget.');
+  // Remove code below if you want to trigger by schedule action and using environment variables.
+  if (!scope[0]) {
+    return context.log("This analysis must be triggered by a widget.");
+  }
 
-  context.log('Downlink analysis started');
+  context.log("Downlink analysis started");
   // Get the environment variables.
-  const env = Utils.envToJson(context.environment);
-  if (!env.account_token) return context.log('Missing "account_token" environment variable');
-  else if (env.account_token.length !== 36) return context.log('Invalid "account_token" in the environment variable');
+  const environment = Utils.envToJson(context.environment);
+  if (!environment.account_token) {
+    return context.log('Missing "account_token" environment variable');
+  } else if (environment.account_token.length !== 36) {
+    return context.log('Invalid "account_token" in the environment variable');
+  }
 
-  // Instance the Account class
-  const account = new Account({ token: env.account_token });
+  // Setup your profile-token from the environment variable
+  // IMPORTANT: You must generate the profile token from your profile settings.
+  //            Any other token will generate an Authorization Denied error.
+  const account = new Account({ token: environment.account_token });
 
   // Get the variables form_payload and form_port sent by the widget/dashboard.
-  const payload = scope.find(x => x.variable === 'form_payload') || { value: env.payload, device: env.device_id };
-  const port = scope.find(x => x.variable === 'form_port') || { value: env.default_PORT };
-  if (!payload || !payload.value || !payload.device) return context.log('Missing "form_payload" in the data scope.');
-  else if (!port || !port.value) return context.log('Missing "form_port" in the data scope.');
+  let payload = scope.find((x) => x.variable === "form_payload");
+  let port = scope.find((x) => x.variable === "form_port");
 
-  const device_id = payload.device; // All variables that trigger the analysis have the "device" parameter, with the TagoIO Device ID.
-  if (!device_id) return context.log('Device parameter <device> not found in the variables sent by the widget/dashboard.');
+  // Setup from environment variable if widget hadn't been used to trigger the analysis.
+  if (!payload) {
+    payload = { value: environment.payload, device: environment.device_id };
+  }
 
-  // Find the token containing the authorization code used.
-  const device_tokens = await account.devices.tokenList(device_id, { page: 1, fields: ['name', 'serie_number', 'last_authorization'], amount: 10 });
-  const token = device_tokens.find(x => x.serie_number && x.last_authorization);
-  if (!token) return context.log("Couldn't find a token with serial/authorization for this device");
+  if (!port) {
+    port = { value: environment.default_PORT };
+  }
 
-  // Get the connector ID from the device
-  const { network: network_id } = await account.devices.info(device_id);
-  if (!network_id) return context.log('Device is not using a network.');
+  // Error to make sure analysis have the information it needs.
+  if (!payload.value || !payload.device) {
+    return context.log('Missing "form_payload" in the data scope.');
+  } else if (!port || !port.value) {
+    return context.log('Missing "form_port" in the data scope o.');
+  }
 
-  // Get the network information with the NS URL for the Downlink
-  const network = await account.integration.networks.info(network_id, ['id', 'middleware_endpoint', "name"]);
-  if (!network.middleware_endpoint) return context.log("Couldn't find a network middleware for this device.");
+  // All variables that trigger the analysis have the "device" parameter, with the TagoIO Device ID.
+  // Otherwise it will get from the environment variable.
+  const device_id = payload.device;
+  if (!device_id) {
+    return context.log("Device key <device> not found in the variables sent by the widget/dashboard.");
+  }
 
-  // Set the parameters for the device. Some NS like Everynet need this.
-  const params = await account.devices.paramList(device_id);
-  let downlink_param = params.find(x => x.key === 'downlink');
-  downlink_param = { id: downlink_param ? downlink_param.id : null, key: 'downlink', value: String(payload.value), sent: false };
-  await account.devices.paramSet(device_id, downlink_param);
-
-  context.log('Trying to send the downlink');
-  const data = {
-    device: token.serie_number,
-    authorization: token.last_authorization,
+  const result = await Utils.sendDownlink(account, device_id, {
     payload: payload.value,
-    port: port.value,
-  };
+    port: Number(port.value),
+    confirmed: false,
+  }).catch((error) => error);
 
-  await axios.post(`https://${network.middleware_endpoint}/downlink`, data)
-   .then((result) => {
-     context.log(`Downlink accepted with status ${result.status}`);
-   })
-   .catch((error) => {
-     context.log(`Downlink failed with status ${error.response.status}`);
-     context.log(error.response.data || JSON.stringify(error));
-   });
+  console.log(result);
 }
 
 module.exports = new Analysis(init);
